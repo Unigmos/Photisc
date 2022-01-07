359482ac0edcea397d276670799aee4b370dea4c
Shaneron <77985354+Unigmos@users.noreply.github.com>
2022-01-07 16:23:19 +0900

    Update main.py

--- main.py
+++ main.py
@@ -3,14 +3,20 @@ from flask import render_template
 
 @app.route('/')
 def index():
-    data = {
-        'title': 1,
-        'price': 1200,
-        'other': "発売停止"}
-    return render_template(
-        "index.html",
-        data = data)
+    return render_template("index.html")
 
 @app.route('/select_template')
 def select_template():
-    return render_template("select_template.html")
+    template_data = [
+        {'image':"images/no_image.png",
+        'description':"テスト用1",
+        'last-updated':"2022年1月7日"},
+
+        {'image':"images/no_image.png",
+        'description':"テスト用2",
+        'last-updatd':"2022年1月7日"}
+    ]
+    return render_template(
+        "select_template.html",
+        template_data = template_data
+        )
