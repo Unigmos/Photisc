448737b48747aa3d579c0579bf3197f09a601890
Shaneron <77985354+Unigmos@users.noreply.github.com>
2022-01-07 18:53:19 +0900

    Update main.py

--- main.py
+++ main.py
@@ -7,16 +7,30 @@ def index():
 
 @app.route('/select_template')
 def select_template():
-    template_data = [
+    template_datas = [
         {'image':"images/no_image.png",
         'description':"テスト用1",
-        'last-updated':"2022年1月7日"},
+        'link_num':1,
+        'last_updated':"2022年1月7日"},
 
         {'image':"images/no_image.png",
         'description':"テスト用2",
-        'last-updatd':"2022年1月7日"}
+        'link_num':2,
+        'last_updated':"2022年1月7日"}
     ]
     return render_template(
         "select_template.html",
-        template_data = template_data
+        template_datas = template_datas
         )
+
+@app.route('/template/temp1')
+def temp1():
+    return render_template("template/temp1.html")
+
+@app.route('/template/temp2')
+def temp2():
+    return render_template("template/temp2.html")
+
+@app.route('/404')
+def page404():
+    return render_template("404.html")
