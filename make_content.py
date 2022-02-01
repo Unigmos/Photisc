68338f7d31a9a59cd1c3fece020f62010b919984
Shaneron <77985354+Unigmos@users.noreply.github.com>
2022-02-02 00:17:02 +0900

    Update make_content.py

--- make_content.py
+++ make_content.py
@@ -8,26 +8,62 @@
 # Copyright:    (c) Shaneron 2022
 #-------------------------------------------------------------------------------
 
-def test():
+def circle_test():
     try:
-        import cv2
-        import numpy as np
+        from matplotlib import pyplot as plt
+        from matplotlib import patches as patch
+        import matplotlib.patches as patches
 
-        height = 400
-        width = 400
-        blank = np.zeros((height, width, 3))
-        blank += [0,255,0][::-1]
-
-        cv2.imwrite('blank.png',blank)
+        img = plt.imread('blank.png')
+        fig, ax = plt.subplots()
+        im = ax.imshow(img)
+        circle = patch.Circle((540, 520), radius=320, transform=ax.transData)
+        im.set_clip_path(circle)
+        ax.axis('off')
+        plt.savefig('clip_circle.png',dpi=150)
 
     except ModuleNotFoundError as NO_MODULE_ERROR:
         print("ModuleNotFoundError:", NO_MODULE_ERROR, "at test")
     except FileNotFoundError as NOT_FOUND_ERROR:
         print("FileNotFoundError:", NOT_FOUND_ERROR, "at test")
 
+def canvas():
+    import cv2
+    import numpy as np
+
+    height = 500
+    width = 1500
+    blank = np.zeros((height, width, 3))
+    # 背景色設定
+    blank += [84,66,59][::-1]
+
+    # 線描画(外枠)
+    cv2.line(blank, (50, 50), (1450, 50), (255, 255, 255), thickness=6, lineType=cv2.LINE_AA, shift=0)
+    cv2.line(blank, (50, 50), (50, 80), (255, 255, 255), thickness=6, lineType=cv2.LINE_AA, shift=0)
+    cv2.line(blank, (1450, 50), (1450, 80), (255, 255, 255), thickness=6, lineType=cv2.LINE_AA, shift=0)
+
+    cv2.line(blank, (50, 450), (1450, 450), (255, 255, 255), thickness=6, lineType=cv2.LINE_AA, shift=0)
+    cv2.line(blank, (50, 450), (50, 420), (255, 255, 255), thickness=6, lineType=cv2.LINE_AA, shift=0)
+    cv2.line(blank, (1450, 450), (1450, 420), (255, 255, 255), thickness=6, lineType=cv2.LINE_AA, shift=0)
+
+    # 線描画(中心円)
+    cv2.circle(blank, (750, 250), 100, (255, 255, 255), thickness=3, lineType=cv2.LINE_AA)
+
+    # 線描画(円左側横線)
+    cv2.line(blank, (50, 245), (650, 245), (255, 255, 255), thickness=3, lineType=cv2.LINE_AA, shift=0)
+    cv2.line(blank, (50, 255), (650, 255), (255, 255, 255), thickness=3, lineType=cv2.LINE_AA, shift=0)
+
+    # 線描画(円右側横線)
+    cv2.line(blank, (850, 245), (1450, 245), (255, 255, 255), thickness=3, lineType=cv2.LINE_AA, shift=0)
+    cv2.line(blank, (850, 255), (1450, 255), (255, 255, 255), thickness=3, lineType=cv2.LINE_AA, shift=0)
+
+    # 画像書き出し
+    cv2.imwrite('blank.png',blank)
+
 def main():
     pass
 
 
 if __name__ == '__main__':
-    test()
+    #circle_test()
+    canvas()
