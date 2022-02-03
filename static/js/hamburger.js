55f592cb3943ecb38ebd5f08ca92914762c8396a
Shaneron <77985354+Unigmos@users.noreply.github.com>
2022-02-03 12:32:23 +0900

    Add files via upload

--- static/js/hamburger.js
+++ static/js/hamburger.js
@@ -1,8 +1,15 @@
 $(function () {
+  var z_index_flag = false;
   $('.navbar_toggle').on('click', function () {
     $(this).toggleClass('open');
     $('.menu').toggleClass('open');
-    $('.to-template-button a').fadeToggle(320);
     $('.main_container').toggleClass('white');
+    if(z_index_flag == false){
+      $('.to-template-button a').css('z-index','0');
+      z_index_flag = true;
+    }else{
+      $('.to-template-button a').css('z-index','30');
+      z_index_flag = false;
+    }
   });
 });
